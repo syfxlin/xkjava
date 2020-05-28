2c4b5faedb6fb8cbab425d5d51c2d3779c914d5e
syfxlin <syfxlin@gmail.com>
2020-05-29 00:16:47 +0800

    :sparkles: feat: Add base64, mac, aes

--- src/main/java/me/ixk/App.java
+++ src/main/java/me/ixk/App.java
@@ -1,12 +1,20 @@
 package me.ixk;
 
-import me.ixk.facades.Config;
-import me.ixk.ioc.Application;
+import me.ixk.utils.Base64;
+import me.ixk.utils.Crypt;
 
 public class App {
 
     public static void main(String[] args) {
-        Application application = Application.createAndBoot();
-        Config.get("app.provider", "123");
+        try {
+            Crypt crypt = new Crypt(
+                Base64.decode("8nv1uWkiIVuSVyQyPnMEOTpzOeZ3CmoqyLA2ZLsfiMM=")
+            );
+            String encrypt = crypt.encrypt("123");
+            System.out.println(encrypt);
+            System.out.println(crypt.decrypt(encrypt));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
