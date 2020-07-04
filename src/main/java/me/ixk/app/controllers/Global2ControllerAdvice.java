5aa624594b5c7f57523f26e8822e6b9b8e33dbc0
syfxlin <syfxlin@gmail.com>
2020-07-04 11:25:54 +0800

    :sparkles: feat: Add WebAppContext

--- src/main/java/me/ixk/app/controllers/Global2ControllerAdvice.java
+++ src/main/java/me/ixk/app/controllers/Global2ControllerAdvice.java
@@ -1,14 +1,35 @@
 package me.ixk.app.controllers;
 
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import me.ixk.framework.annotations.ExceptionHandler;
 import me.ixk.framework.exceptions.Exception;
 
 //@ControllerAdvice
 //@Order(1)
-public class Global2ControllerAdvice {
+@WebFilter(urlPatterns = "/*")
+public class Global2ControllerAdvice implements Filter {
 
     @ExceptionHandler(value = Exception.class)
     public String exception(Exception e) {
         return "error2";
     }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void doFilter(
+        ServletRequest request,
+        ServletResponse response,
+        FilterChain chain
+    )
+        throws IOException, ServletException {
+        System.out.println("filter");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {}
 }
--- src/main/java/me/ixk/framework/server/HttpServer.java
+++ src/main/java/me/ixk/framework/server/HttpServer.java
@@ -4,13 +4,12 @@ import java.io.File;
 import me.ixk.framework.facades.Config;
 import me.ixk.framework.kernel.ErrorHandler;
 import me.ixk.framework.servlet.DispatcherServlet;
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.server.session.DefaultSessionIdManager;
-import org.eclipse.jetty.server.session.SessionHandler;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.webapp.*;
 
 public class HttpServer {
     private final Server server;
@@ -49,29 +48,34 @@ public class HttpServer {
         connector.setPort(port);
         server.addConnector(connector);
 
-        ResourceHandler resourceHandler = new ResourceHandler();
-        resourceHandler.setDirectoriesListed(true);
-        resourceHandler.setResourceBase(resource);
+        WebAppContext context = new WebAppContext();
+        context.setContextPath("/");
+        context.setResourceBase(resource);
+        context.setParentLoaderPriority(true);
 
-        SessionHandler sessionHandler = new SessionHandler();
-
-        ServletHandler servletHandler = new ServletHandler();
-        servletHandler.addServletWithMapping(DispatcherServlet.class, "/*");
+        context.setConfigurations(
+            new Configuration[] {
+                new AnnotationConfiguration(),
+                new WebInfConfiguration(),
+                new WebXmlConfiguration(),
+                new MetaInfConfiguration(),
+                new FragmentConfiguration(),
+                new EnvConfiguration(),
+                new PlusConfiguration(),
+                new JettyWebXmlConfiguration(),
+            }
+        );
+        // 配置 Jetty Embedded 注解扫描包含的路径
+        context.setAttribute(
+            "org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
+            ".*/classes/.*"
+        );
+        context.addServlet(DispatcherServlet.class, "/*");
 
         ErrorHandler errorHandler = new ErrorHandler();
+        context.setErrorHandler(errorHandler);
 
-        ServletContextHandler servletContextHandler = new ServletContextHandler(
-            resourceHandler,
-            "/",
-            sessionHandler,
-            null,
-            servletHandler,
-            errorHandler
-        );
-        servletContextHandler.setResourceBase(resource + "/..");
-
-        server.setHandler(servletContextHandler);
-        server.setSessionIdManager(new DefaultSessionIdManager(server));
+        server.setHandler(context);
 
         return server;
     }
